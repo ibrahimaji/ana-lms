c15014d8ac0ddabb3fc55f1c62b521e28a787843
Ibrahim Aji <ibrahim.aji@live.com>
2023-09-22 09:40:46 +0700

    chore: initializing staging environment

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,8 +1,11 @@
-"use client"
-import { Button } from "@nextui-org/react"
+"use client";
+import { Button } from "@nextui-org/react";
 
 export const Login = () => {
   return (
-    <div><Button color="primary">Hello</Button></div>
-  )
-}
+    <div>
+      <h1>Login</h1>
+      <Button color="primary">Hello</Button>
+    </div>
+  );
+};
